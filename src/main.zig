c717cf4ffeee4156f8a8397c56705d736ff40161
Dominik Schraml <dominik.schraml97@gmail.com>
2025-04-18 01:25:46 +0200

    zig sdl example

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,63 @@
-const std = @import("std");
+const c = @cImport({
+    @cInclude("SDL2/SDL.h");
+});
+const assert = @import("std").debug.assert;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
+        c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    }
+    defer c.SDL_Quit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const screen = c.SDL_CreateWindow("My Game Window", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, 400, 140, c.SDL_WINDOW_OPENGL) orelse
+        {
+            c.SDL_Log("Unable to create window: %s", c.SDL_GetError());
+            return error.SDLInitializationFailed;
+        };
+    defer c.SDL_DestroyWindow(screen);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const renderer = c.SDL_CreateRenderer(screen, -1, 0) orelse {
+        c.SDL_Log("Unable to create renderer: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer c.SDL_DestroyRenderer(renderer);
 
-    try bw.flush(); // don't forget to flush!
-}
+    const zig_bmp = @embedFile("zig.bmp");
+    const rw = c.SDL_RWFromConstMem(zig_bmp, zig_bmp.len) orelse {
+        c.SDL_Log("Unable to get RWFromConstMem: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer assert(c.SDL_RWclose(rw) == 0);
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    const zig_surface = c.SDL_LoadBMP_RW(rw, 0) orelse {
+        c.SDL_Log("Unable to load bmp: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer c.SDL_FreeSurface(zig_surface);
+
+    const zig_texture = c.SDL_CreateTextureFromSurface(renderer, zig_surface) orelse {
+        c.SDL_Log("Unable to create texture from surface: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer c.SDL_DestroyTexture(zig_texture);
+
+    var quit = false;
+    while (!quit) {
+        var event: c.SDL_Event = undefined;
+        while (c.SDL_PollEvent(&event) != 0) {
+            switch (event.type) {
+                c.SDL_QUIT => {
+                    quit = true;
+                },
+                else => {},
+            }
+        }
+
+        _ = c.SDL_RenderClear(renderer);
+        _ = c.SDL_RenderCopy(renderer, zig_texture, null, null);
+        c.SDL_RenderPresent(renderer);
+
+        c.SDL_Delay(17);
+    }
 }
