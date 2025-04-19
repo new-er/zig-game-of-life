32caa8054f85c80b442b4d90657414615f921a84
Dominik Schraml <dominik.schraml97@gmail.com>
2025-04-19 22:06:23 +0200

    fix game rules

--- src/game.zig
+++ src/game.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn allocateTiles(rows: u16, cols: u16) ![][]bool {
+pub fn allocateTiles(rows: usize, cols: usize) ![][]bool {
     const allocator = std.heap.page_allocator;
 
     const outer = try allocator.alloc([]bool, rows);
@@ -15,14 +15,14 @@ pub fn allocateTiles(rows: u16, cols: u16) ![][]bool {
 }
 
 pub fn randomizeTiles(tiles: [][]bool) !void {
-     var prng = std.Random.DefaultPrng.init(blk: {
+    var prng = std.Random.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
         try std.posix.getrandom(std.mem.asBytes(&seed));
         break :blk seed;
     });
     const rand = prng.random();
 
-    for (0.. tiles.len) |row| {
+    for (0..tiles.len) |row| {
         for (0..tiles[row].len) |col| {
             const randomBool = rand.boolean();
             tiles[row][col] = randomBool;
@@ -30,48 +30,55 @@ pub fn randomizeTiles(tiles: [][]bool) !void {
     }
 }
 
-pub fn updateTiles(tiles: [][]bool) !void {
-    // Update the tiles based on the rules of Conway's Game of Life
+pub fn updateTiles(tiles: [][]bool) [][]bool {
     const rows = tiles.len;
     const cols = tiles[0].len;
+    var newTiles = allocateTiles(rows, cols) catch |err| {
+        std.debug.print("Error allocating new tiles: {}\n", .{err});
+        return tiles;
+    };
 
     for (0..rows) |row| {
         for (0..cols) |col| {
             const alive = tiles[row][col];
             const neighbors = countAliveNeighbors(tiles, row, col);
             if (alive) {
-                tiles[row][col] = neighbors == 2 or neighbors == 3;
+                newTiles[row][col] = neighbors == 2 or neighbors == 3;
             } else {
-                tiles[row][col] = neighbors == 3;
+                newTiles[row][col] = neighbors == 3;
             }
         }
     }
+    return newTiles;
 }
 
-pub fn countAliveNeighbors(tiles: [][]bool, x: u64, y: u64) u8 {
+pub fn countAliveNeighbors(tiles: [][]bool, row: u64, col: u64) u8 {
     const rows = tiles.len;
     const cols = tiles[0].len;
     var count: u8 = 0;
 
-    // Check all 8 possible neighbors
-    for (0..3) |i| {
-        for (0..3) |j| {
-            if (i == 1 and j == 1) continue;
-            var xPos = x + i;
-            if (x + i > 1) {
-                xPos = x + i - 1;
-            }
-            var yPos = y + j ;
-            if (y + j > 1) {
-                yPos = y + j - 1;
-            }
-
-            if (xPos < 0 or xPos >= rows or yPos < 0 or yPos >= cols) continue;
-            if (tiles[xPos][yPos]) {
+    for (0..3) |r| {
+        for (0..3) |c| {
+            if (r == 1 and c == 1) continue; // Skip the cell itself
+            const rowI: i64 = @intCast(r);
+            const colI: i64 = @intCast(c);
+            const neighborRow = getIndexSafe(row, rows, rowI - 1);
+            const neighborCol = getIndexSafe(col, cols, colI - 1);
+            if (tiles[neighborRow][neighborCol]) {
                 count += 1;
             }
         }
     }
 
     return count;
+}
+
+fn getIndexSafe(index: usize, length: usize, delta: isize) usize {
+    std.debug.assert(length > 0);
+    const signed_index: isize = @intCast(index);
+    const signed_len: isize = @intCast(length);
+
+    const shifted = signed_index + delta;
+    const wrapped = @mod(shifted, signed_len);
+    return @intCast(wrapped);
 }
--- src/main.zig
+++ src/main.zig
@@ -4,13 +4,23 @@ const c = @cImport({
 const game = @import("game.zig");
 const std = @import("std");
 
+const DragAction = enum {
+    None,
+    Set,
+    Clear,
+};
+
 pub fn main() !void {
-    const tileCountX = 40;
-    const tileCountY = 40;
-    const windowSizeX = 640;
-    const windowSizeY = 640;
+    const tileCountX = 50;
+    const tileCountY = 50;
+    const windowSizeX = 1024;
+    const windowSizeY = 1024;
     const tileSizeX = windowSizeX / tileCountX;
     const tileSizeY = windowSizeY / tileCountY;
+
+    var paused = false;
+    var step = false;
+    var dragAction = DragAction.None;
 
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
         c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
@@ -31,23 +41,73 @@ pub fn main() !void {
     };
     defer c.SDL_DestroyRenderer(renderer);
 
-    const tiles = game.allocateTiles(tileCountX, tileCountY) catch |err| {
+    var tiles = game.allocateTiles(tileCountX, tileCountY) catch |err| {
         c.SDL_Log("Error allocating tiles: %s", "s");
-        return err;
-    };
-    game.randomizeTiles(tiles) catch |err| {
-        c.SDL_Log("Error randomizing tiles: %s", "s");
         return err;
     };
 
     var quit = false;
     var lastUpdate = std.time.milliTimestamp();
+    var lastClick = std.time.milliTimestamp();
     while (!quit) {
         var event: c.SDL_Event = undefined;
         while (c.SDL_PollEvent(&event) != 0) {
             switch (event.type) {
                 c.SDL_QUIT => {
                     quit = true;
+                },
+                c.SDL_MOUSEBUTTONDOWN => {
+                    if (event.button.button == c.SDL_BUTTON_LEFT) {
+                        const x: u64 = @intCast(@divTrunc(event.button.x, tileSizeX));
+                        const y: u64 = @intCast(@divTrunc(event.button.y, tileSizeY));
+                        if (x < tileCountX and y < tileCountY) {
+                            lastClick = std.time.milliTimestamp();
+                            std.debug.print("clicked on tile ({}, {})\n", .{ x, y });
+                            tiles[y][x] = !tiles[y][x];
+                            if (tiles[y][x]) {
+                                dragAction = DragAction.Set;
+                            } else {
+                                dragAction = DragAction.Clear;
+                            }
+                        }
+                    }
+                },
+                c.SDL_MOUSEBUTTONUP => {
+                    if (event.button.button == c.SDL_BUTTON_LEFT) {
+                        dragAction = DragAction.None;
+                    }
+                },
+                c.SDL_MOUSEMOTION => {
+                    if (event.motion.state == c.SDL_BUTTON_LMASK) {
+                        const x: u64 = @intCast(@divTrunc(event.motion.x, tileSizeX));
+                        const y: u64 = @intCast(@divTrunc(event.motion.y, tileSizeY));
+                        if (x < tileCountX and y < tileCountY) {
+                            lastClick = std.time.milliTimestamp();
+                            std.debug.print("dragged on tile ({}, {})\n", .{ x, y });
+                            if (dragAction == DragAction.Set) {
+                                tiles[y][x] = true;
+                            } else if (dragAction == DragAction.Clear) {
+                                tiles[y][x] = false;
+                            }
+                        }
+                    }
+                },
+                c.SDL_KEYDOWN => {
+                    if (event.key.keysym.sym == c.SDLK_SPACE) {
+                        paused = !paused;
+                    }
+                    if (event.key.keysym.sym == c.SDLK_RETURN) {
+                        step = true;
+                    }
+                    if (event.key.keysym.sym == c.SDLK_r) {
+                        game.randomizeTiles(tiles) catch |err| {
+                            c.SDL_Log("Error randomizing tiles: %s", "s");
+                            return err;
+                        };
+                    }
+                    if (event.key.keysym.sym == c.SDLK_ESCAPE) {
+                        quit = true;
+                    }
                 },
                 else => {},
             }
@@ -71,14 +131,23 @@ pub fn main() !void {
                 }
             }
         }
+
+        if (paused or lastClick + 1000 > std.time.milliTimestamp()) {
+            _ = c.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            const pauseRect = c.SDL_Rect{
+                .x = windowSizeX - 20,
+                .y = 0,
+                .w = 20,
+                .h = 20,
+            };
+            _ = c.SDL_RenderFillRect(renderer, &pauseRect);
+        }
         c.SDL_RenderPresent(renderer);
 
-        if (lastUpdate + 100 < std.time.milliTimestamp()) {
+        if ((lastUpdate + 100 < std.time.milliTimestamp() and lastClick + 1000 < std.time.milliTimestamp() and !paused) or step) {
+            step = false;
             lastUpdate = std.time.milliTimestamp();
-            game.updateTiles(tiles) catch |err| {
-                c.SDL_Log("Error updating tiles: %s", err);
-                return err;
-            };
+            tiles = game.updateTiles(tiles);
         }
 
         c.SDL_Delay(17);
